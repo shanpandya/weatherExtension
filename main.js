bd3ac130b769e1a57bc337442f5f0ee2dc684c00
a <a@as-MacBook-Pro.local>
2017-09-22 16:50:42 +0100

    seccond commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const APIKEY = 8420a749e7fef936a7285df4ad75588b;
+// write script to see which page they are on (i.e. which city)
+
+// using data parsed from the BBC website stored as var - lookup against city list and return the city id
+var id = $.getJSON( "city.list.json", function(data) {  
+    // console.log( "JSON Data: " + data);
+    $.each(data, function( index, val ) {
+        if (data[index].name == value) {
+            return data[index].id
+        }
+    });
+});
+
+// list of variables we want to store things in
+var raw;
+
+// create API request based on the city id
+$.getJSON( `api.openweathermap.org/data/2.5/forecast?id=${id}&APPID=${APIKEY}`, function( data ) {
+   raw = data
+ };
