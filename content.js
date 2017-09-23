4970ae444b89cf8a51112df2707fffb225f45b11
Vivien Wee <vivienwee@hotmail.com>
2017-09-23 14:27:21 +0100

    content

--- content.js
+++ content.js
@@ -1,24 +1,27 @@
 var APIKEY = "8420a749e7fef936a7285df4ad75588b";
 // // write script to see which page they are on (i.e. which city)
-var locationName = document.getElementsByClassName('location-name');
-console.log("locationName: " + locationName.innerHTML);
+var locationName = document.getElementsByClassName('location-name')[0].innerHTML;
+console.log("locationName: " + locationName);
 
 // using data parsed from the BBC website stored as var - lookup against city list and return the city id
 // console.log("the script is running");
 // console.log("API key: " + API);
 
 
-var id = $.getJSON(chrome.extension.getURL("city.list.json"), function(data) {  
-    console.log( "JSON Data: " + data);
+var ID = $.getJSON(chrome.extension.getURL("city.list.json"), function(data) {
+    // console.log( "JSON Data: " + data);
     $.each(data, function(index) {
-        console.log("index: " + index)
-        if (data[index].name == locationName.innerHTML) {
+        // console.log("index: " + index)
+        // console.log(data[index].id);
+        if (data[index].name == locationName && data[index].country == "GB") {
             return data[index].id;
         }
     });
 });
 
-console.log("location id: " + id);
+console.log(ID);
+
+console.log("location id: " + ID);
 
 // list of variables we want to store things in
 var raw;
